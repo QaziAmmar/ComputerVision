e00ef4695bc07325e2b78beac26952bd5c4cdde1
QaziAmmar <qaziammar.g@gmail.com>
2019-11-13 23:42:32 +0500

    crop image on the base of non zero pixel count

--- image_registration/openCV_image_stitching.py
+++ image_registration/openCV_image_stitching.py
@@ -11,6 +11,7 @@ import path
 import cv2
 import imutils
 import time
+import numpy as np
 import os
 
 path.init()
@@ -22,6 +23,11 @@ __status__ = "This is working code of python that stitched python images automat
 __description__ = "This Code stitch both video frames and multiple image form same folder. If selection == 1 then " \
                   "this code stitch the images form images folder. other selection will stitch the video frames " \
                   "together."
+
+
+def img_show(img):
+    plt.imshow(img)
+    plt.show()
 
 
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -90,9 +96,40 @@ def read_all_images_name(folder_path):
     return sorted(all_images_name)
 
 
-def remove_100_row_of(image):
+def define_row_col_crop_limit(img):
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+    row, col = gray.shape
+    row_limit = 0
+    col_limit = 0
+    # select row range.
+    for i in range(0, row, 10):
+        temp_img_row = gray[i, :]
+        # Zero counter does not work because the zero has value form 0 - 10.
+        zero_count = np.sum(temp_img_row < 10)
+        non_zero_count = col - zero_count
+        percentage = (zero_count / col) * 100
+        if percentage < 6:
+            row_limit = i
+            break
+
+        # select col range.
+        for j in range(0, col, 10):
+            temp_img_col = gray[:, j]
+            # Zero counter does not work because the zero has value form 0 - 10.
+            zero_count = np.sum(temp_img_col < 10)
+            non_zero_count = col - zero_count
+            percentage = (zero_count / row) * 100
+            if percentage < 6:
+                col_limit = j
+                break
+    return row_limit, col_limit
+
+
+def remove_non_zero_row_col_of(image):
+    row_limit, col_limit = define_row_col_crop_limit(image)
     row, col, ch = image.shape
-    cropped_image = image[100: row - 100, 100: col - 100, :]
+    cropped_image = image[row_limit: row - row_limit, col_limit: col - col_limit, :]
     return cropped_image
 
 
@@ -101,10 +138,8 @@ def read_all_images_form(images_names):
     all_images_array = []
     for image_name in images_names:
         img = cv2.imread(image_name)
-        # img = image_resize(img, height=900)
-        # img = remove_black_region(img)
-        # removing top 100 and bottom 100 row of image.
-        # img = remove_100_row_of(img)
+        img = remove_black_region(img)
+        img = remove_non_zero_row_col_of(img)
         all_images_array.append(img)
     return all_images_array
 
@@ -130,8 +165,8 @@ selection = 1
 out_image_name = "stitched_image.jpg"
 time1 = time.time()
 if selection == 1:
-    results_folder = path.dataset_path + "Malaria_Dataset_self/SHIF_images/test_stitch/"
-
+    results_folder = path.dataset_path + "Malaria_Dataset_self/SHIF_images/miscrscope_panaroma.2/"
+    print("[INFO] key frames extraction of images...")
     # This function perform stitching on images.
     images_name = read_all_images_name(results_folder)
     key_frames = read_all_images_form(images_name)
@@ -167,75 +202,9 @@ time3 = time.time()
 cv2.imwrite(results_folder + out_image_name, stitched)
 
 # convert BGR to RGB so that color shown right.
+stitched = cv2.cvtColor(stitched, cv2.COLOR_BGR2RGB)
 plt.imshow(stitched)
 plt.show()
 
 print('key frames extraction:', time2 - time1, ' sec')
 print('Stitching: ', time3 - time2, ' sec')
-
-# if the status is '0', then OpenCV successfully performed image
-# stitching
-# if status == 0:
-#     # check to see if we supposed to crop out the largest rectangular
-#     # region from the stitched image
-#     if 10 > 0:
-#         # create a 10 pixel border surrounding the stitched image
-#         print("[INFO] cropping...")
-#         stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10,
-#                                       cv2.BORDER_CONSTANT, (0, 0, 0))
-#
-#         # convert the stitched image to grayscale and threshold it
-#         # such that all pixels greater than zero are set to 255
-#         # (foreground) while all others remain 0 (background)
-#         gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
-#         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
-#         # find all external contours in the threshold image then find
-#         # the *largest* contour which will be the contour/outline of
-#         # the stitched image
-#         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-#                                 cv2.CHAIN_APPROX_SIMPLE)
-#         cnts = imutils.grab_contours(cnts)
-#         c = max(cnts, key=cv2.contourArea)
-#
-#         # allocate memory for the mask which will contain the
-#         # rectangular bounding box of the stitched image region
-#         mask = np.zeros(thresh.shape, dtype="uint8")
-#         (x, y, w, h) = cv2.boundingRect(c)
-#         cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
-#         # create two copies of the mask: one to serve as our actual
-#         # minimum rectangular region and another to serve as a counter
-#         # for how many pixels need to be removed to form the minimum
-#         # rectangular region
-#         minRect = mask.copy()
-#         sub = mask.copy()
-#
-#         # keep looping until there are no non-zero pixels left in the
-#         # subtracted image
-#         while cv2.countNonZero(sub) > 0:
-#             # erode the minimum rectangular mask and then subtract
-#             # the thresholded image from the minimum rectangular mask
-#             # so we can count if there are any non-zero pixels left
-#             minRect = cv2.erode(minRect, None)
-#             sub = cv2.subtract(minRect, thresh)
-#             # find contours in the minimum rectangular mask and then
-#             # extract the bounding box (x, y)-coordinates
-#             cnts = cv2.findContours(minRect.copy(), cv2.RETR_EXTERNAL,
-#                                     cv2.CHAIN_APPROX_SIMPLE)
-#             cnts = imutils.grab_contours(cnts)
-#             c = max(cnts, key=cv2.contourArea)
-#             (x, y, w, h) = cv2.boundingRect(c)
-#
-#             # use the bounding box coordinates to extract the our final
-#             # stitched image
-#             stitched = stitched[y:y + h, x:x + w]
-#             # write the output stitched image to disk
-#             # cv2.imwrite(args["output"], stitched)
-#
-#             # display the output stitched image to our screen
-#             # cv2.imshow("Stitched", stitched)
-#             # cv2.waitKey(0)
-#
-#         # otherwise the stitching failed, likely due to not enough keypoints)
-#         # being detected
-#         else:
-#             print("[INFO] image stitching failed ({})".format(status))
